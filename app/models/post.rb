4b2b69e1a21a4e882d0c9d3cb176c7d45d8853c3
Chely Ho <adept_inept@yahoo.com>
2018-04-12 04:06:06 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,13 @@ class Post < ActiveRecord::Base
 
   accepts_nested_attributes_for :categories
 
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+    end
+  end
+
   def commentors
     self.comments.map(&:user).uniq
   end
