b6b199de193aacd205af70bbf079abad0282cd9a
Chely Ho <adept_inept@yahoo.com>
2018-04-12 05:48:44 +0000

    automatically backed up by learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,7 +6,7 @@ class Comment < ActiveRecord::Base
 
   def user_attributes=(user_attributes)
 
-    self.user = User.find_or_create_by(user_attributes) if !user_attributes.empty?
+    self.user = User.find_or_create_by(user_attributes) if !user_attributes.values.first.empty?
     byebug
   end
 
